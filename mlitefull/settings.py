7e55ece7155f6f6462a48fc8388852f40d3c5d2d
phmtuan <phmtuan313@gmail.com>
2019-06-17 22:03:14 +0700

    debug to false

--- mlitefull/settings.py
+++ mlitefull/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'v4*(vl0=0w(bq^*$vv#i-uq+)1rtz1-w0(dw##^3pim9qmkg6v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['134.209.101.186']
 # '134.209.101.186'
